dbab965d5409ea11f5ca9bfa5334dc5c37bd9ab8
Rossil2012 <40714231+Rossil2012@users.noreply.github.com>
2020-11-16 19:15:28 +0800

    dev(backend): Finish functions for single user By wukanzhen

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"backend/repository"
+	"backend/router"
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/middleware/logger"
+	"github.com/kataras/iris/v12/middleware/recover"
+)
 
+func main() {
+	app := NewApp()
+
+	if err := app.Run(iris.Addr("0.0.0.0:8080"), iris.WithoutServerError(iris.ErrServerClosed)); err != nil {
+		panic("Failed to Start Server!")
+	}
+}
+
+func NewApp() *iris.Application {
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+
+	app.Use(recover.New())
+	app.Use(logger.New())
+
+	router.SetRouter(app)
+	repository.InitDBConn()
+
+	return app
 }
