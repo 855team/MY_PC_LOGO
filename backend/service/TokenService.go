d5a275dba6c6762851d0f09ef913517f17d9c742
Rossil2012 <40714231+Rossil2012@users.noreply.github.com>
2021-05-05 17:59:44 +0800

    test: add TestGetProjectSimple By Rossil2012

--- backend/service/TokenService.go
+++ backend/service/TokenService.go
@@ -16,7 +16,7 @@ type grant struct {
 	Term	int64
 }
 
-func CheckToken(token string) uint {
+var CheckToken = func(token string) uint {
 	if token == "0000000000000000000000000000000000000000000000000000000000000000" {
 		return 1
 	} else if token == "1111111111111111111111111111111111111111111111111111111111111111" {
@@ -37,7 +37,7 @@ func CheckToken(token string) uint {
 	}
 }
 
-func NewToken(uid uint, username string) (token string) {
+var NewToken = func(uid uint, username string) (token string) {
 	origin := fmt.Sprintf("%d %s %s", uid, username, time.Now().Format("2006-01-02 15:04:05"))
 	token = fmt.Sprintf("%x", sha256.Sum256([]byte(origin)))
 	tokenMap.Store(token, grant{
--- backend/tests/stub_test.go
+++ backend/tests/stub_test.go
@@ -1,25 +1,43 @@
 package tests
 
 import (
+	"backend/dao"
+	"backend/model"
+	"backend/service"
 	"backend/utils"
 	"github.com/prashantv/gostub"
 	"testing"
 )
 
 func TestStub(t *testing.T) {
-	// 函数打桩
-	stubFunc := gostub.Stub(&utils.UintListContains, func(list []uint, element uint) bool {
+	// 函数打桩，需要函数体
+	stubFunc1 := gostub.Stub(&utils.UintListContains, func(list []uint, element uint) bool {
 		return list[0] == element
 	})
-	defer stubFunc.Reset() // 复原，一定要
+	defer stubFunc1.Reset() // 复原，一定要
 
 	l := []uint{1, 2, 3}
-	t.Log("stubFunc", utils.UintListContains(l, 2))
+	t.Log("stubFunc1", utils.UintListContains(l, 2))
+
+	// 函数打桩，不需要函数体
+	stubFunc2 := gostub.StubFunc(&utils.UintListContains, true)
+	defer stubFunc2.Reset()
+
+	t.Log("stubFunc2", utils.UintListContains(l, 0))
 
 	// 变量打桩
 	str := "old"
 	stubVar := gostub.Stub(&str, "new")
-	defer stubVar.Reset() // 一定要
+	defer stubVar.Reset()
 
 	t.Log("stubVar", str)
+}
+
+func TestGetProjectSimple(t *testing.T) {
+	defer gostub.StubFunc(&utils.UintListContains, true).
+		StubFunc(&service.CheckToken, uint(100)).
+		StubFunc(&dao.GetPidsByUid, []uint{1, 2, 3}).
+		StubFunc(&dao.GetProjectWithFilesByPid, model.Project{Pid: 0, Name: "test", Files: nil}).Reset()
+	success, msg, data := service.GetProject(utils.GetProjectParams{Pid: 100})
+	t.Log(success, msg, data)
 }
